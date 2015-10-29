59f4ebd747b337b4e90cd028fd8db53e8043acb0
muhdamrullah <muneorion@gmail.com>
2015-10-29 16:21:23 +0000

    Update object_trigger.py

--- object_trigger.py
+++ object_trigger.py
@@ -6,15 +6,18 @@ import time
 from SimpleCV import Image, Camera
 
 def take_photo():
-    cam = Camera()
-    img = cam.getImage()
-    img.save("object.png")
+    pygame.camera.init()
+    cam = pygame.camera.Camera(pygame.camera.list_cameras()[0])
+    cam.start()
+    img = cam.get_image()
+    pygame.image.save(img, "photo.bmp")
+    pygame.camera.quit()
 
 def load_recognizer():
     subprocess.call("Find-Object.exe --console --object box.png --Camera/6useTcpCamera 'true' --Camera/8port 5000 --General/port 6000", shell=True)
 
 def compare_photo():
-    subprocess.call("find_object-tcpRequest.exe --json 'out.json' --scene object.png --out 5000 --in 6000", shell=True)
+    subprocess.call("find_object-tcpRequest.exe --json 'out.json' --scene photo.bmp --out 5000 --in 6000", shell=True)
 
 def read_json():
     if os.path.isfile("out.json") == True:
